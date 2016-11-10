b79e35a87087dbab60fbfb0a4b66fa30f9b8c99e
alessioricco <alessio.ricco@gmail.com>
2016-11-10 10:17:15 +0000

    new media formats in feeds

--- app/src/main/java/it/alessioricco/tsflickr/models/ItemMediaDescription.java
+++ app/src/main/java/it/alessioricco/tsflickr/models/ItemMediaDescription.java
@@ -1,5 +1,8 @@
 package it.alessioricco.tsflickr.models;
 
+import android.util.Patterns;
+import android.webkit.URLUtil;
+
 import java.io.Serializable;
 
 import lombok.Getter;
@@ -8,7 +11,67 @@ import lombok.Setter;
 /**
  * represent the media description
  * "media": {"m":"https://farm6.staticflickr.com/5807/30248454164_f09d4360d2_m.jpg"},
+ *
+ * image formats are described here:
+ * https://www.flickr.com/services/api/misc.urls.html
+ *
+ *
+ *
+ s	quadratino 75 x 75
+ q	large square 150x150
+ t	miniatura, 100 sul lato più lungo
+ m	piccolo, 240 sul lato più lungo
+ n	small, 320 on longest side
+ -	medio, 500 sul lato più lungo
+ z	medio 640, 640 sul lato più lungo
+ c	medio 800, 800 sul lato più lungo†
+ b	grande, 1024 sul lato più lungo*
+ h	grandi 1600, 1600 nel lato più lungo†
+ k	grandi 2048, 2048 nel lato più lungo†
+ o	immagine originale jpg, gif o png, in base al formato di origine
+
+ assumptions to investigate and test:
+ we have lowercase url
+ we always receive m values
+ we always have .jpg
+
+ *
  */
 public class ItemMediaDescription implements Serializable {
+
+    // it will contains the picture url in small format
     @Getter @Setter private String m;
+
+    private static String defaultExtension = ".jpg";
+
+    public String getOriginal() {
+        return m.replace("_m.jpg", defaultExtension);
+    }
+
+    public String getSmallSquare() {
+        return m.replace("_m.jpg", "_s.jpg");
+    }
+
+    public String getMedium() {
+        return m.replace("_m.jpg", "_z.jpg");
+    }
+
+    public String getBig() {
+        return m.replace("_m.jpg", "_b.jpg");
+    }
+
+    public boolean isValid() {
+        return isValid(m);
+    }
+
+    private boolean isValid(final String url) {
+        if (url == null || url.length() ==0) {
+            return false;
+        }
+        if (!(Patterns.WEB_URL.matcher(url).matches())) {
+            return false;
+        }
+        return true;
+    }
+
 }
--- app/src/test/java/it/alessioricco/tsflickr/models/TestItemMediaDescription.java
+++ app/src/test/java/it/alessioricco/tsflickr/models/TestItemMediaDescription.java
@@ -1,21 +1,60 @@
 package it.alessioricco.tsflickr.models;
 
-import org.junit.Test;
+import android.util.Patterns;
+import android.webkit.URLUtil;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowResources;
+
+import it.alessioricco.tsflickr.TestEnvironment;
 import it.alessioricco.tsflickr.mocks.MockItemMediaDescriptionFactory;
+import it.alessioricco.tsflickr.util.CustomRobolectricTestRunner;
+import rx.android.BuildConfig;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
-
+@Config(shadows = { ShadowResources.class },
+        sdk = TestEnvironment.sdk,
+        constants = BuildConfig.class,
+        manifest = TestEnvironment.manifest)
+@RunWith(CustomRobolectricTestRunner.class)
 public class TestItemMediaDescription {
     @Test
     public void testMediaIsNotEmptyAfterSerialization() throws Exception {
 
         final ItemMediaDescription item = MockItemMediaDescriptionFactory.createItemMediaDescription();
         assertThat(item).isNotNull();
+        assertThat(item.isValid()).isTrue();
 
         assertThat(item.getM()).isNotNull();
         assertThat(item.getM()).isNotEmpty();
+
+    }
+
+    @Test
+    public void testMediaFormats() throws Exception {
+
+        final ItemMediaDescription item = MockItemMediaDescriptionFactory.createItemMediaDescription();
+
+        assertThat(item.getOriginal().replace(".jpg","_m.jpg")).isEqualTo(item.getM());
+        assertThat(item.getOriginal().endsWith(".jpg"));
+        assertThat(item.getSmallSquare().endsWith("_s.jpg"));
+        assertThat(item.getMedium().endsWith("_z.jpg"));
+        assertThat(item.getMedium().endsWith("_b.jpg"));
+
+        assertThat(item.getM()).isNotEqualTo(item.getOriginal());
+        assertThat(item.getM()).isNotEqualTo(item.getSmallSquare());
+        assertThat(item.getM()).isNotEqualTo(item.getMedium());
+        assertThat(item.getM()).isNotEqualTo(item.getBig());
+
+        // valid url (robolectric)
+        assertThat(Patterns.WEB_URL.matcher(item.getOriginal()).matches()).isTrue();
+        assertThat(Patterns.WEB_URL.matcher(item.getSmallSquare()).matches()).isTrue();
+        assertThat(Patterns.WEB_URL.matcher(item.getMedium()).matches()).isTrue();
+        assertThat(Patterns.WEB_URL.matcher(item.getBig()).matches()).isTrue();
+        assertThat(Patterns.WEB_URL.matcher(item.getM()).matches()).isTrue();
 
     }
 }
